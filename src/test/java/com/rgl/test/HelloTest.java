84e050804fbe90d9949c69272d93b9b7ba3217c5
banxiaomo <674408398@qq.com>
2021-03-31 17:00:48 +0800

    学习项目练习. 添加练习步骤的说明.

--- src/test/java/com/rgl/test/HelloTest.java
+++ src/test/java/com/rgl/test/HelloTest.java
@@ -1,10 +1,14 @@
 package com.rgl.test;
+
 import org.junit.Test;
 import org.junit.Assert;
-public class HelloTest{
-    @Test
-    public void sayHallo(){
-        System.out.println("----junitTest,sayHello----");
-	Assert.assertEquals("ok","ok");
-    }
+
+public class HelloTest {
+	@Test
+	public void sayHallo() {
+		System.out.println("----junitTest,sayHello----");
+		System.out.println("head");
+		Assert.assertEquals("ok", "ok");
+		System.out.println("tail");
+	}
 }
--- src/test/java/com/rgl/test/TestHello.java
+++ src/test/java/com/rgl/test/TestHello.java
@@ -1,6 +1,13 @@
 package com.rgl.test;
-public class TestHello{
-    public void testHello(){
-        System.out.println("baseTest,no junit");
-    }
+
+public class TestHello {
+	public void testHello() {
+		System.out.println("baseTest,no junit,testHello");
+	}
+	public void simpletest() {
+		System.out.println("baseTest,no junit,simpletest");
+	}
+	public void simpleTest() {
+		System.out.println("baseTest,no junit,simpleTest");
+	}
 }
